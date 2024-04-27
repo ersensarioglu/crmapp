5a9630c79e4c99b307262eda88ec2817d54d27a3
Ersen Sarioglu <ersen.sarioglu@gmail.com>
2024-04-27 14:16:45 +0100

    static and templates setting with first app template

--- website/webapp/views.py
+++ website/webapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Welcome to my website!")
+    return render(request, 'webapp/index.html')
