569c2b83a42a6b251746019ead6b6189b572eade
Ersen Sarioglu <ersen.sarioglu@gmail.com>
2024-04-28 16:42:02 +0100

    login page created

--- website/webapp/views.py
+++ website/webapp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate
 from .forms import CreateUserForm, LoginForm
 
 def home(request):
@@ -10,6 +12,21 @@ def register(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
-            # return redirect('')
-    context = {'form':form}
+            return redirect('my-login')
+    context = {'form': form}
     return render(request, 'webapp/register.html', context)
+
+def my_login(request):
+    form = LoginForm()
+    if request.method == "POST":
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                # return redirect('')
+    context = {'form': form}
+    return render(request, 'webapp/my-login.html', context)
+        
