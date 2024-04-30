8bdb26fe19a9b00ba9c321f665c52039c9dc2b56
Ersen Sarioglu <ersen.sarioglu@gmail.com>
2024-04-30 18:47:39 +0100

    Update record

--- website/webapp/views.py
+++ website/webapp/views.py
@@ -49,6 +49,18 @@ def create_record(request):
     context = {'form': form}
     return render(request, 'webapp/create-record.html', context)
 
+@login_required(login_url='my-login')
+def update_record(request, pk):
+    record = Record.objects.get(id=pk)
+    form = UpdateRecordForm(instance=record)
+    if request.method == "POST":
+        form = UpdateRecordForm(request.POST, instance=record)
+        if form.is_valid():
+            form.save()
+            return redirect('dashboard')
+    context = {'form': form}
+    return render(request, 'webapp/update-record.html', context)
+
 def user_logout(request):
     auth.logout(request)
     return redirect('my-login')
